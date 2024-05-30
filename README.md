f561fa4bf2a78bec04c45c00db6b0b653a763aef
Brig1tte <korantengbrigitte96@gmail.com>
2024-05-30 11:14:57 +0000

    My Portfolio Project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-Webstack Portfolio Project:
+Final ALX Front-End Project Work:
+
+Webstack Portfolio Project
 My Portfolio Website Project
--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 let typingEffect = new Typed(".typedText", {
-  strings: ["Business-oriented", "Persistent", "Affable"],
+  strings: ["FrontEnd Developer", "Customer Relations Exec."],
   loop: true,
   typeSpeed: 100,
   backSpeed: 80,
